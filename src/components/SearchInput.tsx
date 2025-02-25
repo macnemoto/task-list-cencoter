5e82c6f3dfba2ca4f08aaeb4b9ecbbc1bc0ca0de
Marcos Pirela <marcosj465@gmail.com>
2025-02-25 10:09:58 -0400

    feat(SearchInput): improve component design and functionality

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Search } from 'lucide-react'
+import { useRef } from "react";
 
 const SearchInput: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return (
-        <div className="bg-red-500 rounded-full flex">
-            <div className="p-4  w-1/6">
-                <Search />
-            </div>
-            <input className=" w-5/6 rounded-r-[9999px]" type="text" placeholder="Busca un nombre o precio" />
+        <div className="flex relative">
+            <Search onClick={() => inputRef.current?.focus()} className="text-[#FAFAFA] absolute left-3 top-1/2 -translate-y-1/2" />
+            <input ref={inputRef} className="bg-[#09090B] w-5/6 border-1 border-[#27272A] rounded-md py-2 pl-12 text-white" type="text" placeholder="Busca un nombre o precio" />
         </div>
     )
 }
