3830209c6b69a01d40461ebd2788900d80da4bab
Marcos Pirela <marcosj465@gmail.com>
2025-02-25 10:27:31 -0400

    fix: correct input size in the SearchInput component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -8,7 +8,7 @@ const SearchInput: React.FC = () => {
     return (
         <div className="flex relative">
             <Search onClick={() => inputRef.current?.focus()} className="text-[#FAFAFA] absolute left-3 top-1/2 -translate-y-1/2" />
-            <input ref={inputRef} className="bg-[#09090B] w-5/6 border-1 border-[#27272A] rounded-md py-2 pl-12 text-white" type="text" placeholder="Busca un nombre o precio" />
+            <input ref={inputRef} className="bg-[#09090B] w-full border-1 border-[#27272A] rounded-md py-2 pl-12 text-white" type="text" placeholder="Busca un nombre o precio" />
         </div>
     )
 }
